8c16a17e4a899257c8614cf4010b27f0b6e5f367
thembones79 <michal.szumnarski@gmail.com>
2023-04-09 14:55:04 +0200

    added map

--- src/main.rs
+++ src/main.rs
@@ -180,32 +180,57 @@ pub fn create_player(world: &mut World, position: Position) {
 
 // Initialize the level
 pub fn initialize_level(world: &mut World) {
-    create_player(
-        world,
-        Position {
-            x: 0,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
-    create_wall(
-        world,
-        Position {
-            x: 1,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
-    create_box(
-        world,
-        Position {
-            x: 2,
-            y: 0,
-            z: 0, // we will get the z from the factory functions
-        },
-    );
+    const MAP: &str = "
+    N N W W W W W W
+    W W W . . . . W
+    W . . . B . . W
+    W . . . . . . W 
+    W . P . . . . W
+    W . . . . . . W
+    W . . S . . . W
+    W . . . . . . W
+    W W W W W W W W
+    ";
+
+    load_map(world, MAP.to_string());
 }
 
+pub fn load_map(world: &mut World, map_string: String) {
+    let rows: Vec<&str> = map_string.trim().split('\n').map(|x| x.trim()).collect();
+    for (y, row) in rows.iter().enumerate() {
+        let columns: Vec<&str> = row.split(' ').collect();
+
+        for (x, column) in columns.iter().enumerate() {
+            let position = Position {
+                x: x as u8,
+                y: y as u8,
+                z: 0,
+            };
+
+            match *column {
+                "." => create_floor(world, position),
+                "W" => {
+                    create_floor(world, position);
+                    create_wall(world, position);
+                }
+                "P" => {
+                    create_floor(world, position);
+                    create_player(world, position);
+                }
+                "B" => {
+                    create_floor(world, position);
+                    create_box(world, position);
+                }
+                "S" => {
+                    create_floor(world, position);
+                    create_box_spot(world, position);
+                }
+                "N" => (),
+                c => panic!("unrecognizes map item {c}"),
+            }
+        }
+    }
+}
 pub fn main() -> GameResult {
     let mut world = World::new();
     register_components(&mut world);
