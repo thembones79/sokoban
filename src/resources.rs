295d3118a257bf512b5f913351d7cf0d15c28c76
thembones79 <michal.szumnarski@gmail.com>
2023-04-16 14:06:18 +0200

    started with Gameplay system

--- src/resources.rs
+++ src/resources.rs
@@ -8,6 +8,24 @@ pub struct InputQueue {
     pub keys_pressed: Vec<KeyCode>,
 }
 
+#[derive(Default)]
+pub struct Gameplay {
+    pub state: GameplayState,
+    pub moves_count: u32,
+}
+
+pub enum GameplayState {
+    Playing,
+    Won,
+}
+
+impl Default for GameplayState {
+    fn default() -> Self {
+        Self::Playing
+    }
+}
+
 pub fn register_resources(world: &mut World) {
-    world.insert(InputQueue::default())
+    world.insert(InputQueue::default());
+    world.insert(Gameplay::default());
 }
