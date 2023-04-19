12d3a09051c0974005b1dbfa2cf4430966244c66
thembones79 <michal.szumnarski@gmail.com>
2023-04-20 00:35:09 +0200

    wip

---
+++ src/systems/gameplay_state_system.rs
@@ -0,0 +1,27 @@
+use specs::{Join, ReadStorage, System, Write};
+use std::collections::HashMap;
+
+use crate::{
+    components::{Box, BoxSpot, Position},
+    resources::{Gameplay, GameplayState},
+};
+
+pub struct GameplayStateSystem {}
+
+impl<'a> System<'a> for GameplayStateSystem {
+    type SystemData = (
+        Write<'a, Gameplay>,
+        ReadStorage<'a, Position>,
+        ReadStorage<'a, Box>,
+        ReadStorage<'a, BoxSpot>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (mut gameplay_state, positions, boxes, box_spots) = data;
+
+        let boxes_by_position: HashMap<(u8, u8), &Box> = (&positions, &boxes)
+            .join()
+            .map(|t| ((t.0.x, t.0.y), t.1))
+            .collect::<HashMap<_, _>>();
+    }
+}
